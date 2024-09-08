0c92ea5ebbccd0ee9601d048b6383b84b5cf7007
DiegoAlvarez04 <di703444@ucf.edu>
2024-09-08 23:46:17 +0000

    more changes

--- fracturing.c
+++ fracturing.c
@@ -8,19 +8,18 @@ double x2=6;
 double y2=6;
 double distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 
-printf("Point 1 (%.2lf,%.2lf), Point 2 (%.2lf,%.2lf), distance: %.2lf\n",x1,y1,x2,y2,distance);
+printf("Point #1 entered: x1=%.1lf,y1=%.lf\n",x1,y1);
+printf("Point #2 entered: x2= %.2lf,y2=%.2lf\n",x2,y2);
+printf("The distance between the two points is: %.1lf\n",distance);
 
 return distance;
 }
 double calculatePerimeter(){
-double x1=3;
-double y1=3;
-double x2=6;
-double y2=6;
-double perimeter = 2*PI*(calculateDistance()/2); //perimeter of a circle is 2pir, where r is diameter/2
+double distance= calculateDistance();
+double perimeter = 2*PI*(distance/2); //perimeter of a circle is 2pir, where r is diameter/2
 
-printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, Perimeter: %lf\n",x1,y1,x2,y2,perimeter);
-
+//printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, Perimeter: %lf\n",x1,y1,x2,y2,perimeter);
+printf("The perimeter of the city encompassed by your request is: %.1lf\n",perimeter);
 return 3.5;
 }
 double calculateArea(){
@@ -28,27 +27,23 @@ double x1=3;
 double y1=3;
 double x2=6;
 double y2=6;
-double area = 2*PI*pow((calculateDistance()/2),2); //area of a circle is 2pir^2, where r is diameter/2
-printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, area: %lf\n",x1,y1,x2,y2,area);
-
+double distance= calculateDistance();
+double area = 2*PI*pow(distance/2,2); //area of a circle is 2pir^2, where r is diameter/2
+//printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, area: %lf\n",x1,y1,x2,y2,area);
+printf("The area of the city encompassed by your request is: %.1lf\n",area);
 return 2.0;
 }
 
 double calculateWidth(){
-double x1=3;
-double y1=3;
-double x2=6;
-double y2=6;
-printf("Point 1 (%lf,%lf), Point 2 (%lf,%lf), width: %lf)",x1,y1,x2,y2,calculateDistance());
-
+double distance= calculateDistance();
+//printf("Point 1 (%lf,%lf), Point 2 (%lf,%lf), width: %lf)",x1,y1,x2,y2,calculateDistance());
+printf("The width of the city encompassed by your request is: %.1lf\n",distance);
 return 1.0;
 }
 double calculateHeight(){
-double x1=3;
-double y1=3;
-double x2=6;
-double y2=6;
-printf("Point 1 (%lf,%lf), Point 2 (%lf,%lf), height: %lf)",x1,y1,x2,y2,calculateDistance());
+double distance= calculateDistance();
+//printf("Point 1 (%lf,%lf), Point 2 (%lf,%lf), height: %lf)",x1,y1,x2,y2,calculateDistance());
+printf("The height of the city encompassed by your request is: %.1lf\n",distance);
 return 1;
 }
 
