1493461aa921266f1e5d12f83dd6685b14a14d7d
DiegoAlvarez04 <di703444@ucf.edu>
2024-09-09 00:26:25 +0000

    more changes

--- fracturing.c
+++ fracturing.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
+
+
+
 double calculateDistance(){
-double x1=3;
-double y1=3;
-double x2=6;
-double y2=6;
+double x1,x2,y1,y2;
+ x1=3;
+ y1=3;
+x2=6;
+y2=6;
 double distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 
-printf("Point #1 entered: x1=%.1lf,y1=%.lf\n",x1,y1);
-printf("Point #2 entered: x2= %.2lf,y2=%.2lf\n",x2,y2);
+printf("Point #1 entered: x1=%.1lf; y1=%.1lf\n",x1,y1);
+printf("Point #2 entered: x2= %.1lf;y2=%.1lf\n",x2,y2);
 printf("The distance between the two points is: %.1lf\n",distance);
+
 
 return distance;
 }
 double calculatePerimeter(){
-double distance= calculateDistance();
+double distance = calculateDistance();
 double perimeter = 2*PI*(distance/2); //perimeter of a circle is 2pir, where r is diameter/2
 
 //printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, Perimeter: %lf\n",x1,y1,x2,y2,perimeter);
@@ -28,8 +33,8 @@ double y1=3;
 double x2=6;
 double y2=6;
 double distance= calculateDistance();
-double area = 2*PI*pow(distance/2,2); //area of a circle is 2pir^2, where r is diameter/2
-//printf("Point 1: x1=%lf,y1=%lf), Point 2: x2=%lf,y2=%lf, area: %lf\n",x1,y1,x2,y2,area);
+double area = PI*pow(distance/2,2); //area of a circle is 2pir^2, where r is diameter/2
+//printf("Point 1: x1=%.1lf,y1=%.1lf), Point 2: x2=%.1lf,y2=%.1lf, area: %.1lf\n",x1,y1,x2,y2,area);
 printf("The area of the city encompassed by your request is: %.1lf\n",area);
 return 2.0;
 }
